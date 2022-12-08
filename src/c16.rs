5ebaa5ea2bc99f767365d43621a2dd25104dafc5
SG <sarah.gould@gmail.com>
2022-12-07 19:36:30 -0800

    add png-to-c16

--- src/c16.rs
+++ src/c16.rs
@@ -1,4 +1,4 @@
-use bytes::{ Bytes, Buf };
+use bytes::{ Bytes, BytesMut, Buf, BufMut };
 use image::{ RgbaImage, Rgba };
 
 struct FileHeader {
@@ -42,7 +42,7 @@ fn read_image_header(buffer: &mut Bytes) -> Option<ImageHeader> {
 }
 
 fn read_image_data(contents: &[u8], header: &ImageHeader, pixel_format: u32) -> RgbaImage {
-	let mut img = RgbaImage::new(header.width as u32, header.height as u32);
+	let mut image = RgbaImage::new(header.width as u32, header.height as u32);
 	for (y, line_offset) in (&header.line_offsets).iter().enumerate() {
 		let mut buffer = Bytes::copy_from_slice(contents);
 		buffer.advance(*line_offset as usize);
@@ -55,18 +55,18 @@ fn read_image_data(contents: &[u8], header: &ImageHeader, pixel_format: u32) -> 
 				if run_type == 1 && buffer.remaining() >= (run_length * 2).into() {
 					for i in 0..run_length {
 						let color = read_pixel_data(buffer.get_u16_le(), pixel_format);
-						img.put_pixel((x + i) as u32, y as u32, color);
+						image.put_pixel((x + i) as u32, y as u32, color);
 					}
 				} else if run_type == 0 {
 					for i in 0..run_length {
-						img.put_pixel((x + i) as u32, y as u32, Rgba([0, 0, 0, 0]));
+						image.put_pixel((x + i) as u32, y as u32, Rgba([0, 0, 0, 0]));
 					}
 				}
 				x += run_length;
 			}
 		}
 	}
-	return img;
+	return image;
 }
 
 fn read_pixel_data(pixel: u16, pixel_format: u32) -> Rgba<u8> {
@@ -88,20 +88,135 @@ fn read_pixel_data(pixel: u16, pixel_format: u32) -> Rgba<u8> {
 	}
 }
 
-pub fn decode(contents: &[u8]) {
+pub fn decode(contents: &[u8]) -> Vec<RgbaImage> {
+	let mut images: Vec<RgbaImage> = Vec::new();
 	let mut buffer = Bytes::copy_from_slice(contents);
 	if let Some(file_header) = read_file_header(&mut buffer) {
-		println!("pixel format: {:?}, image count: {:?}", file_header.pixel_format, file_header.image_count);
 		let mut image_headers: Vec<ImageHeader> = Vec::new();
-		for i in 0..file_header.image_count {
+		for _ in 0..file_header.image_count {
 			if let Some(image_header) = read_image_header(&mut buffer) {
 				image_headers.push(image_header);
 			}
 		}
-		for (i, image_header) in image_headers.iter().enumerate() {
-			println!("IMAGE {:?}: width {:?}, height {:?}", i, image_header.width, image_header.height);
-			let img = read_image_data(&contents, &image_header, file_header.pixel_format);
-			img.save("test.png");
+		for image_header in image_headers {
+			let image = read_image_data(&contents, &image_header, file_header.pixel_format);
+			images.push(image);
 		}
 	}
+	return images;
+}
+
+fn write_file_header(buffer: &mut BytesMut, image_count: u16) {
+	buffer.put_u32_le(3); // 565 format
+	buffer.put_u16_le(image_count);
+}
+
+fn write_image_header(buffer: &mut BytesMut, width: u16, height: u16, line_offsets: Vec<u32>) {
+	if line_offsets.len() > 1 {
+		println!("IMAGE HEADER");
+		println!("first line offset: {}", line_offsets[0]);
+		buffer.put_u32_le(line_offsets[0]);
+		buffer.put_u16_le(width);
+		buffer.put_u16_le(height);
+		if line_offsets.len() > 2 {
+			for line_offset in line_offsets[1..].iter() {
+				buffer.put_u32_le(*line_offset);
+			}
+		}
+	}
+}
+
+fn write_image_data(image: &RgbaImage, image_offset: u32) -> (BytesMut, Vec<u32>) {
+	let mut buffer = BytesMut::new();
+	let mut line_offsets: Vec<u32> = Vec::new();
+	let mut last_line_offset = image_offset;
+	let mut last_buffer_size: u32 = 0;
+
+	for y in 0..image.height() {
+		line_offsets.push(last_line_offset);
+		let mut transparent_run = 0;
+		let mut color_run: Vec<Rgba<u8>> = Vec::new();
+		for x in 0..image.width() {
+			let pixel = image.get_pixel(x, y);
+			if pixel[3] == 0 {
+				// transparent pixel
+				if color_run.len() > 0 {
+					// end active color run
+					write_color_run(&mut buffer, &color_run);
+					color_run.clear();
+				}
+				transparent_run += 1;
+			} else {
+				// color pixel
+				if transparent_run > 0 {
+					// end active transparent run
+					write_transparent_run(&mut buffer, transparent_run);
+					transparent_run = 0;
+				}
+				color_run.push(*pixel);
+			}
+		}
+		// wrap up active run
+		if color_run.len() > 0 {
+			write_color_run(&mut buffer, &color_run);
+		} else if transparent_run > 0 {
+			write_transparent_run(&mut buffer, transparent_run);
+		}
+		// end of line
+		buffer.put_u16_le(0);
+		last_line_offset += (buffer.len() as u32) - (last_buffer_size as u32);
+		last_buffer_size = buffer.len() as u32;
+	}
+	// end of image
+	buffer.put_u16_le(0);
+
+	return (buffer, line_offsets);
+}
+
+fn write_color_run(buffer: &mut BytesMut, color_run: &Vec<Rgba<u8>>) {
+	let run_header = (1 & 0x1) | ((color_run.len() << 1) & 0xfffe);
+	buffer.put_u16_le(run_header as u16);
+	for pixel in color_run {
+		write_pixel_data(buffer, pixel[0].into(), pixel[1].into(), pixel[2].into());
+	}
+}
+
+fn write_transparent_run(buffer: &mut BytesMut, transparent_run: u16) {
+	let run_header: u16 = (0 & 0x1) | ((transparent_run << 1) & 0xfffe);
+	buffer.put_u16_le(run_header);
+}
+
+fn write_pixel_data(buffer: &mut BytesMut, r: u16, g: u16, b: u16) {
+	let pixel_data: u16 = ((r << 8) & 0xf800) | ((g << 3) & 0x07e0) | ((b >> 3) & 0x001f);
+	buffer.put_u16_le(pixel_data);
+}
+
+pub fn encode(images: Vec<RgbaImage>) -> BytesMut {
+	// write file header to buffer
+	let mut buffer = BytesMut::new();
+	write_file_header(&mut buffer, images.len() as u16);
+
+
+	// calculate initial offset of image data (= file header + image headers)
+	let mut image_offset = buffer.len() as u32;
+	for image in &images {
+		let image_header_size = (image.height() * 4) + 4;
+		image_offset += image_header_size as u32;
+	}
+
+	// get image data
+	let mut image_headers_buffer = BytesMut::new();
+	let mut images_buffer = BytesMut::new();
+	for image in images {
+		let (image_buffer, line_offsets) = write_image_data(&image, image_offset);
+		write_image_header(&mut image_headers_buffer, image.width() as u16, image.height() as u16, line_offsets);
+		image_offset += image_buffer.len() as u32;
+		images_buffer.unsplit(image_buffer);
+	}
+
+	// write image data to buffer
+	buffer.unsplit(image_headers_buffer);
+	buffer.unsplit(images_buffer);
+
+	return buffer;
 }
--- src/main.rs
+++ src/main.rs
@@ -3,37 +3,79 @@ mod c16;
 
 use std::env;
 use std::fs;
+use std::fs::File;
+use std::io::prelude::*;
+use regex::Regex;
+use image::RgbaImage;
+use image::io::Reader as ImageReader;
 
 fn main() {
 	let args: Vec<String> = env::args().collect();
 	if let (Some(action), Some(filepath)) = (&args.get(1), &args.get(2)) {
-		match action.as_str() {
-			"compile" => {
-				match fs::read_to_string(filepath) {
-					Ok(contents) => {
-						println!("COMPILE AGENT FROM: {}", filepath);
-						agent::parse_source(&contents);
-					},
-					Err(error) => {
-						println!("ERROR: {}", error)
+		let filepath_pattern = Regex::new(r"^(.*[\\/])?(.+)\.(.+)$").unwrap();
+		if let Some(captures) = filepath_pattern.captures(filepath) {
+
+			let path = &captures[1];
+			let filename = &captures[2];
+			let extension = &captures[3];
+
+			match action.as_str() {
+				"compile" => {
+					match fs::read_to_string(filepath) {
+						Ok(contents) => {
+							println!("COMPILE AGENT FROM: {}", filepath);
+							agent::parse_source(&contents);
+						},
+						Err(error) => {
+							println!("ERROR: {}", error)
+						}
 					}
-				}
-			},
-			"c16_to_png" => {
-				match fs::read(filepath) {
-					Ok(contents) => {
-						println!("READ C16 FROM: {}", filepath);
-						c16::decode(&contents);
-					},
-					Err(error) => {
-						println!("ERROR: {}", error)
+				},
+				"c16_to_png" => {
+					match fs::read(filepath) {
+						Ok(contents) => {
+							println!("READ C16 FROM: {}", filepath);
+							let images = c16::decode(&contents);
+							for (i, image) in images.iter().enumerate() {
+								let output_filepath = format!("{}{}-{}.png", path, filename, i);
+								match File::open(&output_filepath) {
+									Err(_why) => {
+										image.save(&output_filepath);
+									},
+									Ok(_file) => println!("ERROR: file already exists: {}", &output_filepath)
+								}
+							}
+						},
+						Err(error) => {
+							println!("ERROR: {}", error)
+						}
 					}
-				}
-			},
-			"png_to_c16" => {
-				// if any additional args, add to list of filepaths
-			},
-			_ => ()
+				},
+				"png_to_c16" => {
+					let output_filepath = format!("{}{}.c16", path, filename); // TODO: use regex to ignore any end numbers
+					match File::open(format!("{}{}.c16", path, filename)) {
+						Err(_why) => {
+							let mut images: Vec<RgbaImage> = Vec::new();
+							for i in 2..args.len() {
+								if let Ok(image) = ImageReader::open(&args[i]) {
+									if let Ok(image) = image.decode() {
+										images.push(image.into_rgba8());
+									}
+								}
+							}
+							let c16_data = c16::encode(images);
+							match File::create(&output_filepath) {
+								Err(why) => println!("ERROR: {} cannot be created: {}", &output_filepath, why),
+								Ok(mut file) => {
+									file.write_all(&c16_data);
+								}
+							}
+						},
+						Ok(_file) => println!("ERROR: file already exists: {}", &output_filepath)
+					}
+				},
+				_ => ()
+			}
 		}
 	}
 }
